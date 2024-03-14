19845cfdab375c1bf549f598ad0235d1ad773730
Azamat Gaparov <agaparovkg@gmail.com>
2024-03-13 17:08:15 -0700

    new changes

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // const url = "https://restcountries.com/v3.1/all";
 import { data } from "./data.js";
-// import { combine } from "./data.js";
-// import { text1, text2, data1 } from "./data.js";
-// const data = require('./data.js')
 
-// console.log("data", combine[0], combine[1], combine[2]);
-console.log("data", combine);
+// Q1 - Total Population - Number
+// Q2 - # of independent countries - Number
+// Q3 - Population of the country with ccn3: 275 - Number
+// Q4 - # of countries that have more than 2 capital cities - Number
+// Q5 - Index of country Liberia inside countries array - Number
+// Q6 - Country that has max Population - Name
+// Q7 - Frequency count of all countries for each continents - Object
+
+const q1 = document.querySelector("#q1 td:last-child");
+const q2 = document.querySelector("#q2 td:last-child");
+const q3 = document.querySelector("#q3 td:last-child");
+const q4 = document.querySelector("#q4 td:last-child");
+const q5 = document.querySelector("#q5 td:last-child");
+const q6 = document.querySelector("#q6 td:last-child");
+const q7 = document.querySelector("#q7 td:last-child");
